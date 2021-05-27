ef73f55893cf0a25b1153b6dacc8d36035eabd84
Eugeny Alekseenko <puncong@mail.ru>
2021-05-27 15:19:51 +1000

    crud тесты

--- store/tests/test_api.py
+++ store/tests/test_api.py
@@ -1,3 +1,6 @@
+import json
+
+from django.contrib.auth.models import User
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
@@ -7,6 +10,7 @@ from store.serializers import BooksSerializer
 
 class BooksApiTestCase(APITestCase):
     def setUp(self):
+        self.user = User.objects.create(username='test_username')
         self.book_1 = Book.objects.create(name='Test Book 1',
                                           price=99,
                                           author_name='Author 1')
@@ -27,7 +31,7 @@ class BooksApiTestCase(APITestCase):
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual(serializer_data, response.data)
 
-    def test_get_filter(self):
+    def test_filter(self):
         url = reverse('book-list')
         response = self.client.get(url, data={'price': 999})
         serializer_data = BooksSerializer([self.book_2,
@@ -36,7 +40,7 @@ class BooksApiTestCase(APITestCase):
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual(serializer_data, response.data)
 
-    def test_get_search(self):
+    def test_search(self):
         url = reverse('book-list')
         response = self.client.get(url, data={'search': 'Author 1'})
         serializer_data = BooksSerializer([self.book_1,
@@ -45,7 +49,7 @@ class BooksApiTestCase(APITestCase):
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual(serializer_data, response.data)
 
-    def test_get_ordering(self):
+    def test_ordering(self):
         url = reverse('book-list')
         response = self.client.get(url, data={'ordering': 'author_name'})
         serializer_data = BooksSerializer([self.book_1,
@@ -54,3 +58,39 @@ class BooksApiTestCase(APITestCase):
                                           many=True).data
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual(serializer_data, response.data)
+
+    def test_post(self):
+        self.assertEqual(3, Book.objects.all().count())
+        url = reverse('book-list')
+        data = {
+            "name": "Book 0",
+            "price": "150",
+            "author_name": "Author 0"
+        }
+        json_data = json.dumps(data)
+        self.client.force_login(self.user)
+        response = self.client.post(url,
+                                   data=json_data,
+                                   content_type='application/json')
+
+        self.assertEqual(status.HTTP_201_CREATED, response.status_code)
+        self.assertEqual(4, Book.objects.all().count())
+
+    def test_put(self):
+        url = reverse('book-detail', args=(self.book_1.id,))
+        data = {
+            "name": self.book_1.name,
+            "price": "777",
+            "author_name": self.book_1.author_name
+        }
+        json_data = json.dumps(data)
+        self.client.force_login(self.user)
+        response = self.client.put(url,
+                                   data=json_data,
+                                   content_type='application/json')
+
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        # пересоздаем объект из базы в тест, иначе не пройдет test_ok
+        # self.book_1 = Book.objects.get(id=self.book_1.id)
+        self.book_1.refresh_from_db()
+        self.assertEqual(777, self.book_1.price)
--- store/tests/test_serializers.py
+++ store/tests/test_serializers.py
@@ -6,19 +6,25 @@ from store.serializers import BooksSerializer
 
 class BookSerializerTestCase(TestCase):
     def test_ok(self):
-        book_1 = Book.objects.create(name='Test Book 1', price=99)
-        book_2 = Book.objects.create(name='Test Book 2', price=999)
+        book_1 = Book.objects.create(name='Test Book 1',
+                                     price=99,
+                                     author_name='Author 1')
+        book_2 = Book.objects.create(name='Test Book 2',
+                                     price=999,
+                                     author_name='Author 2')
         data = BooksSerializer([book_1, book_2], many=True).data
         expected_data = [
             {
                 'id': book_1.id,
                 'name': 'Test Book 1',
-                'price': '99.00'
+                'price': '99.00',
+                'author_name': 'Author 1'
             },
             {
                 'id': book_2.id,
                 'name': 'Test Book 2',
-                'price': '999.00'
+                'price': '999.00',
+                'author_name': 'Author 2'
             },
         ]
         self.assertEqual(expected_data, data)
